6a015c6e053cc3e7147c7c1f07c447ca7d7d9f37
Igor Bastos Alencar <alencar29htcss@gmail.com>
2025-12-12 22:53:30 -0300

    feat: created title component and change size font, adjusted responsive break points

--- src/components/ui/title-nav/title.jsx
+++ src/components/ui/title-nav/title.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import ILogo from "../../../assets/imgs/logo/logo-med-dent.png";
 import S from "../../../styles/components/ui/title-header.module.scss";
 
-export const TitleNav = ({ color }) => {
+export const TitleNav = ({ color, label = "Médicos & Dentista" }) => {
   return (
     <Link to="/">
       <div className={S.title_page}>
@@ -10,7 +10,13 @@ export const TitleNav = ({ color }) => {
           src={ILogo}
           alt="Logo de um coração branco, com um fundo verde quadrado"
         />
-        <h2 style={{ color: color }}>Médicos & Dentista</h2>
+        <h2
+          style={{
+            color: color,
+          }}
+        >
+          {label}
+        </h2>
       </div>
     </Link>
   );
