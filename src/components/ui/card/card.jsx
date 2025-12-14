6cabc5d795d6d0a3057f99eb723757e3b2abe6fa
Igor Bastos Alencar <alencar29htcss@gmail.com>
2025-12-14 19:39:34 -0300

    fix: change name props to "key" from "keyCard"

--- src/components/ui/card/card.jsx
+++ src/components/ui/card/card.jsx
@@ -1,9 +1,8 @@
 import S from "../../../styles/components/ui/card.module.scss";
 
-export const Card = ({ key, type, title, description }) => {
-  console.log(S[type]);
+export const Card = ({ keyCard, type, title, description }) => {
   return (
-    <article key={key} className={S[type]}>
+    <article key={keyCard} className={S[type]}>
       <h3>{title}</h3>
       <p>{description}</p>
     </article>
