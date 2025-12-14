ee050fb2cfa6ca4ebaa74760ea9dca67a8a6683b
Igor Bastos Alencar <alencar29htcss@gmail.com>
2025-12-14 19:38:32 -0300

    feat: added new menu than mobile responsive

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,47 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../../ui/button/button";
 import { TitleNav } from "../../ui/title-nav/title";
-import { MenuBurger } from "../../ui/menu-burger/menu-burger";
 
 import S from "../../../styles/components/header.module.scss";
 import IBurger from "../../../assets/imgs/icons/menu-burger.png";
 
 const Header = () => {
   const [isOpenMenu, setOpenMenu] = useState(false);
-  const handleOpenMenuBurger = () => setOpenMenu(!isOpenMenu);
+  const [isSelect, setSelect] = useState("Home");
 
-  const handleCloseMenuBurger = (id, e) => {
-    const { id: idTarget } = e.target;
-    if (idTarget === id) setOpenMenu(false);
-  };
+  const handleOpenMenuBurger = () => setOpenMenu(!isOpenMenu);
+  const handleClick = (name) => setSelect(name);
+
+  useEffect(() => {
+    const handleCloseModal = () => setOpenMenu(false);
+    window.addEventListener("resize", handleCloseModal);
+    return () => window.removeEventListener("resize", handleCloseModal);
+  }, []);
 
   return (
     <header>
-      <TitleNav color="black" />
+      <section>
+        <TitleNav color="black" />
 
-      <button className={S.burger_btn} onClick={() => handleOpenMenuBurger()}>
-        <img
-          src={IBurger}
-          alt="Icone de Menu Burger(3 barras pretas com espaço entre elas), com o fundo transparente"
-        />
-      </button>
+        <button
+          className={isOpenMenu ? S.burger_btn__animating : S.burger_btn}
+          onClick={() => handleOpenMenuBurger()}
+        >
+          <img
+            src={IBurger}
+            alt="Icone de Menu Burger(3 barras pretas com espaço entre elas), com o fundo transparente"
+          />
+        </button>
 
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/seja-voluntario">
-              <Button type="default" label="Seja Voluntário" />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/seja-voluntario">
+                <Button type="default" label="Seja Voluntário" />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
 
       {isOpenMenu && (
-        <MenuBurger onClose={(id, e) => handleCloseMenuBurger(id, e)} />
+        <nav className={S.menu_burger}>
+          <ul>
+            <li>
+              <Link
+                onClick={() => handleClick("Home")}
+                className={isSelect === "Home" ? S.Click : ""}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => handleClick("BeAVol")}
+                className={isSelect === "BeAVol" ? S.Click : ""}
+                to="/seja-voluntario"
+              >
+                Seja Voluntário
+              </Link>
+            </li>
+          </ul>
+        </nav>
       )}
     </header>
   );
